a6309e9c0931ac8474652bc35ad44bbb29a10500
mihailgaberov <mihail.gaberov@gmail.com>
2019-02-05 16:56:29 +0100

    Doing Gatsby tutorial - blog part.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
