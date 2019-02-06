fb853e84f285efec4bf71d5bba3223b59336710e
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-06 15:28:19 +0100

    Changed reading time icon.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,8 @@
 export function formatReadingTime(minutes) {
   let cups = Math.round(minutes / 5);
-  return `${new Array(cups || 1).fill('☕️').join('')} ${minutes} min read`;
+  return `${new Array(cups || 1).fill('⌛️').join('')} ${minutes} min read`;
 }
 
-// `lang` is optional and will default to the current user agent locale
 export function formatPostDate(date, lang) {
   if (typeof Date.prototype.toLocaleDateString !== 'function') {
     return date;
