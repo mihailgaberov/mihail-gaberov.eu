830fa158e279b53248ee604ed9d202fc8b0bb0ba
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-06 13:25:36 +0100

    Added pages and slugs.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
-import React from "react"
-import { graphql } from "gatsby"
 import { css } from "@emotion/core"
-import { rhythm } from "../utils/typography"
+import { graphql, Link } from "gatsby"
+import React from "react"
 import Layout from "../components/layout"
+import { rhythm } from "../utils/typography"
+
 
 export default ({ data }) => {
   console.log(data)
@@ -20,6 +21,14 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `
+              }
+            >
             <h3
               css={css`
                 margin-bottom: ${rhythm(1 / 4)};
@@ -35,6 +44,7 @@ export default ({ data }) => {
               </span>
             </h3>
             <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
@@ -52,6 +62,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+            fields {
+            slug
           }
           excerpt
         }
