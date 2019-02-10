fd3028a44f92b3caf9bf10738f2a922374ac1446
mihailgaberov <mihail.gaberov@gmail.com>
2019-02-10 11:29:19 +0100

    Added emoji instead of bullets.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => <h1>Footer</h1>;
+export default props => <h1>props.footerText</h1>;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import './about.module.css';
 
 export default () => (
   <Layout>
