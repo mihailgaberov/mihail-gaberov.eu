df5d6b61e1403cd5b443418a663683c6b3d9033b
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-12 16:30:55 +0100

    Show article title in the meta title.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import { graphql } from 'gatsby';
+import SEO from "../components/SEO"
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <Layout>
+      <SEO
+        title={post.frontmatter.title}
+      />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
