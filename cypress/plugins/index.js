d926f5777fd48ab932b3c202e2de4d075b5b0daf
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-15 11:51:13 +0100

    Added env dependent configs for Cypress.

---
+++ cypress/plugins/index.js
@@ -0,0 +1,17 @@
+// promisified fs module
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile (file) {
+  const pathToConfigFile = path.resolve(__dirname, 'config', `cypress.${file}.json`)
+
+  return fs.readJson(pathToConfigFile)
+}
+
+// plugins file
+module.exports = (on, config) => {
+  // accept a configFile value or use development by default
+  const file = config.env.configFile || 'development'
+
+  return getConfigurationByFile(file)
+}
