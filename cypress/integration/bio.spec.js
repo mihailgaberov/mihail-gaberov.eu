531adebf59865f8c55e9d13f7f453b673f29b7eb
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-28 16:15:28 +0100

    Added e2e tests for Bio component.

--- cypress/integration/bio.spec.js
+++ cypress/integration/bio.spec.js
@@ -8,23 +8,23 @@
 */
 
 describe('Bio area', () => {
+  beforeEach(() => {
+    cy.visit('')
+  });
+
   it('should have bottom margin of 2.88rem', () => {
-    cy.visit('');
     cy.get('[data-test=bio]').should('have.css', 'margin-bottom').and('eq', '51.84px');
   });
 
   it('should have a rounded picture', () => {
-    cy.visit('');
     cy.get('[data-test=profile-pic]').should('have.css', 'border-radius').and('eq', '50%');
   });
 
   it('should render paragraph with short text', () => {
-    cy.visit('');
     cy.get('p').should('contain', 'Personal blog by Mihail Gaberov. Learning by sharing.');
   });
 
   it('should render a link to LinkedIn', () => {
-    cy.visit('');
     cy.get('a')
       .contains('Mihail Gaberov')
       .should('have.attr', 'href', 'https://www.linkedin.com/in/mihail-gaberov-6a73b03a/')
